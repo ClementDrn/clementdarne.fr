24a84e801e9a322f7a2c0bc3263030b736ce8db0
Clément Darne <clementdarne8888@gmail.com>
2024-04-24 16:04:38 +0200

    v2: WIP

---
+++ sql/init.sql
@@ -0,0 +1,21 @@
+-- Scripts that generate database for clementdarne.fr
+
+-- Create database
+DROP DATABASE IF EXISTS clementdarne_fr;
+CREATE DATABASE clementdarne_fr;
+
+-- Use database
+USE clementdarne_fr;
+
+-- Create tables
+CREATE TABLE IF NOT EXISTS Projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(63) NOT NULL,
+    directory VARCHAR(63) NOT NULL,
+    visible BOOLEAN DEFAULT TRUE,
+    released BOOLEAN DEFAULT TRUE,
+    github_path VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    start_year INT NOT NULL,
+    end_year INT VARCHAR(255) NOT NULL
+);
